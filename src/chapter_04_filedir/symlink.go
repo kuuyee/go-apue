add7b90cadc0908a953fe0606d35ac292d00adf6
kuuyee <kuuyee@gmail.com>
2015-11-02 11:16:01 +0800

    修正symlink输出说明

--- src/chapter_04_filedir/symlink.go
+++ src/chapter_04_filedir/symlink.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	err := os.Symlink("foo", "foolink")
 	if err != nil {
-		fmt.Fprintf(os.Stdout, "创建文件报错：", err)
+		fmt.Fprintf(os.Stdout, "创建文件连接报错：", err)
 	}
 
 	fmt.Println(os.Readlink("foolink"))
